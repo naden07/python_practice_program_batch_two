e70ae4d16a79948eae0e6131183a36c81fc0b191
jlnshly <jelaineashleyramos@gmail.com>
2026-03-13 04:26:54 +0800

    not equal check for numbers done

---
+++ not_equal_if_numbers_are_not_the_same.py
@@ -0,0 +1,9 @@
+number_1 = float(input("Enter the first number: "))
+number_2 = float(input("Enter the second number: "))
+
+if number_1 != number_2:
+    print(f"{number_1} is not equal to {number_2}")
+else:
+    print(f"{number_1} is equal to {number_2}")
+
+    
