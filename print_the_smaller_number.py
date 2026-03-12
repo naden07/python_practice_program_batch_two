5605ac6d0b22ffe0302d814f029ce85ddc8f4a3a
jlnshly <jelaineashleyramos@gmail.com>
2026-03-13 04:16:43 +0800

    printing the smaller number done

---
+++ print_the_smaller_number.py
@@ -0,0 +1,6 @@
+#input first number
+number_1 = float(input("Enter the first number: "))
+number_2 = float(input("Enter the second number: "))
+
+#output
+print(f"The smaller number is {min(number_1, number_2)}")
